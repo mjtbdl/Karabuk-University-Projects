a5a5e8413127d09d5a9993b69deb57c3f31915dc
Mecit Bedel <77532469+mjtbdl@users.noreply.github.com>
2023-03-27 16:19:55 +0300

    Add files via upload

---
+++ Programming-Languages2/Lab questions/Week3/RecursiveEvenElementsOfArray.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int even(int a[], int n){
+    if (n == 0){
+        return 0;
+    }
+    else if (a[n-1] % 2 == 0){
+        return 1 + even(a, n-1);
+    }
+    else{
+        return even(a, n-1);
+    }
+}
+
+int main(){
+    int i, n, a[100];
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    printf("Even elements of the array: %d", even(a, n));
+}
